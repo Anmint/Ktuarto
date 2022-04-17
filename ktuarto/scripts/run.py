3758a09ae4614df2e78d7353043f4fd3ce1b601a
Anmint <anmint@outlook.com>
2022-04-17 18:17:54 +0900

    add modified C value

--- ktuarto/scripts/run.py
+++ ktuarto/scripts/run.py
@@ -14,7 +14,11 @@ from ..AI.montecarlo_ai import Montecarlo
 from ..AI.montecarlo_choose_diagonal_lines_first import MontecarloChooseDiagonalLinesFirst
 from ..AI.montecarlo_choose_places_other_than_diagonal_lines_first import MontecarloChoosePlacesOtherThanGiagonalLinesFirst
 from ..AI.montecarlo_ai_PrioritizedDraw import MontecarloPrioritizedDraw
+from ..AI.montecarlo_ai_PrioritizedDrawC1 import MontecarloPrioritizedDrawC1
+from ..AI.montecarlo_ai_PrioritizedDrawC3 import MontecarloPrioritizedDrawC3
 from ..AI.montecarlo_ai_UnifiedUcbCalc import MontecarloUnifiedUcbCalc
+from ..AI.montecarlo_ai_UnifiedUcbCalcC1 import MontecarloUnifiedUcbCalcC1
+from ..AI.montecarlo_ai_UnifiedUcbCalcC3 import MontecarloUnifiedUcbCalcC3
 
 import click
 import sys
